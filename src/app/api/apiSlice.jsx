59490136d422c14d0782c3d6c16a8cd4a09c7806
demigod <213689604+defxq@users.noreply.github.com>
2025-11-10 15:08:18 +0630

    fixed baseUrl

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { logOut, setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "https://notesify-api.onrender.com",
+    baseUrl: "https://notesify-api-6iln.onrender.com",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
